e04d71a06f887c2fc6257cf44072ed6c1abaee23
clarissaD <physicscla@gmail.com>
2024-06-06 16:49:05 -0300

    Create retorna0.c

---
+++ retorna0.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+int main() {
+int i = -1, j = -1;
+int w1,w1;
+w1 = (i > 0)&&(j<0)||(i<0)&&(j>0);
+w2 = (i<=0)||(j>=0)&&(i>=0)||(j<=0);
+printf("%d",w1==w2);
+return 0;
+}
+
+/*if e else nao coloca ponto e vigula */
+  
