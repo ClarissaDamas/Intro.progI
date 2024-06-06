c599fa23a388c02dd41ed9b910d965cf19f63a31
clarissaD <physicscla@gmail.com>
2024-06-06 17:38:50 -0300

    Create tamanho.c

---
+++ tamanho.c
@@ -0,0 +1,5 @@
+int main() {
+char t[20] = "ABCDEFGHIJK";
+int s = strlen(t); TAMANHO
+t[3] = "\0"; estou trocando a terceira posicao por \0, entao so vai chegar ate \0 apagou o restante
+ se valia 11 agr vai valer 3 
