aeeacc2eebb81775a033fbd1287dc1f83436ab96
clarissaD <physicscla@gmail.com>
2024-06-24 18:53:15 -0300

    Create revendo.c

---
+++ revendo.c
@@ -0,0 +1,19 @@
+Elabore na linguagem C um módulo recursivo que receba dois
+números inteiros, como parâmetros, 
+//retorne o
+resultado do somatório de todos os números
+contidos no intervalo aberto delimitado pelos
+números fornecidos
+//Em seguida, construa um
+algoritmo que se utilize de forma eficaz do módulo
+elaborado.
+
+#include stdio.h
+#include stdio.th
+function main()
+{
+  int main ()
+  
+
+
+}
