c084d07e61d1c5778a17d3c0df865ae4fa1baecf
clarissaD <physicscla@gmail.com>
2024-07-02 17:58:17 -0300

    Birthday.py

---
+++ aniversatiantes.py
@@ -0,0 +1,16 @@
+
+
+class aniversariantes:
+    def __init__(self,nome, dia, mes):
+        self.nome = nome
+        self.dia = dia
+        self.mes = mes
+        print(self.nome,self.dia,self.mes)
+
+aniversariantes("paola",2,12)
+aniversariantes("paola",2,12)
+aniversariantes("paola",2,12)
+aniversariantes("paola",2,12)
+aniversariantes [1,2,3,4,5,6,7,8,9,10,11,12]
+print(aniversariantes.nome)
+print('fim')
